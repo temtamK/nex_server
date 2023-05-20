b96181cc1b7684a50ab08c9883cf87709bd02525
temtamk <temtamk@kakao.com>
2023-05-20 21:16:14 +0900

    [Feat] Change logic

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Post()
+  createUser(@Body() address): void {
+    this.appService.createUser(address);
+    this.appService.mintNFT(address);
+  }
+
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  callEvent(): string {
+    return this.appService.callEvent();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,66 @@
 import { Injectable } from '@nestjs/common';
+import { exec } from 'child_process';
+import { ethers, Signer } from 'ethers';
+import { PrismaService } from 'src/database/prisma.service';
+import fs from 'fs';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  private provider = new ethers.JsonRpcProvider(process.env.EVM_CHAIN_RPC_URL);
+  private jsonFile = fs.readFileSync('../web3/nex.json', 'utf8');
+  private jsonData = JSON.parse(this.jsonFile);
+  private contract = new ethers.Contract(
+    '0x63f569459170DDbf2fD9F26ed10b27202c2d9Faf',
+    jsonData.abi,
+    this.provider,
+  );
+
+  constructor(private prisma: PrismaService) {}
+
+  async createUser(address: string): Promise<void> {
+    await this.prisma.user.create({
+      data: {
+        address: address,
+      },
+    });
+  }
+
+  async mintNFT(address: string): Promise<void> {
+    //Gas relay mining
+    exec(
+      `cast send \
+    --private-key=acd1c67b10ca079d3dada5a0bda75fa51b119cae2266205372541792a2bf7eda \
+    --rpc-url=${process.env.EVM_CHAIN_RPC_URL} \
+    ${process.env.GASLESS_COUNTER_RECIPIENT_CONTRACT_ADDRESS} \
+    "mint()"`,
+      (err, stdout, stderr) => {
+        if (err) {
+          console.log(`exec error: ${err}`);
+          return;
+        }
+
+        console.log(`stdout: ${stdout}`);
+        console.error(`stderr: ${stderr}`);
+      },
+    );
+
+    // Update metadata
+    const lastTokenId = (await this.contract.getTotalSupply()) - 1;
+    await this.contract.setBaseURI(
+      lastTokenId,
+      'ipfs://QmXEyPiccKn1ifXyJQohnRgtanxpvBdwzPbE2na4LvwQx3',
+    );
+
+    // Transfer NFT
+    const signer = new ethers.Wallet(
+      process.env.RUNNER_PRIVATE_KEY,
+      this.provider,
+    );
+
+    await this.contract.safeTransferFrom(signer.address, address, lastTokenId);
+  }
+
+  async callEvent(address: string) {
+    this.contract.eventFrize(address);
   }
 }
