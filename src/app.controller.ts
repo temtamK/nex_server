695ea4735125ffa2d8ccfa4b62af81f6b989e28a
temtamk <temtamk@kakao.com>
2023-05-21 05:01:13 +0900

    [Feat] Add dNFT

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,18 +1,23 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Post()
-  createUser(@Body() address): void {
-    this.appService.createUser(address);
-    this.appService.mintNFT(address);
+  @Post('createUser')
+  createUser(@Body() body) {
+    this.appService.createUser(body);
+    this.appService.mintNFT(body);
   }
 
-  @Get()
-  callEvent(): string {
-    return this.appService.callEvent();
+  @Post('mint')
+  mint(@Body() body) {
+    this.appService.mintNFT(body);
+  }
+
+  @Post('event')
+  callEvent(@Body() eventRequest) {
+    this.appService.callEvent(eventRequest);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,66 +1,141 @@
 import { Injectable } from '@nestjs/common';
-import { exec } from 'child_process';
-import { ethers, Signer } from 'ethers';
+import { ethers } from 'ethers';
 import { PrismaService } from 'src/database/prisma.service';
-import fs from 'fs';
+import * as fs from 'fs';
 
 @Injectable()
 export class AppService {
-  private provider = new ethers.JsonRpcProvider(process.env.EVM_CHAIN_RPC_URL);
-  private jsonFile = fs.readFileSync('../web3/nex.json', 'utf8');
-  private jsonData = JSON.parse(this.jsonFile);
-  private contract = new ethers.Contract(
-    '0x63f569459170DDbf2fD9F26ed10b27202c2d9Faf',
-    jsonData.abi,
+  private provider = ethers.getDefaultProvider(process.env.EVM_CHAIN_RPC_URL);
+  private wallet = new ethers.Wallet(
+    process.env.RUNNER_PRIVATE_KEY,
     this.provider,
   );
-
   constructor(private prisma: PrismaService) {}
 
-  async createUser(address: string): Promise<void> {
+  async createUser(body): Promise<void> {
     await this.prisma.user.create({
       data: {
-        address: address,
+        address: body.address,
+        grade: 1,
+        tokenId: 9999,
       },
     });
   }
 
-  async mintNFT(address: string): Promise<void> {
-    //Gas relay mining
-    exec(
-      `cast send \
-    --private-key=acd1c67b10ca079d3dada5a0bda75fa51b119cae2266205372541792a2bf7eda \
-    --rpc-url=${process.env.EVM_CHAIN_RPC_URL} \
-    ${process.env.GASLESS_COUNTER_RECIPIENT_CONTRACT_ADDRESS} \
-    "mint()"`,
-      (err, stdout, stderr) => {
-        if (err) {
-          console.log(`exec error: ${err}`);
-          return;
-        }
-
-        console.log(`stdout: ${stdout}`);
-        console.error(`stderr: ${stderr}`);
-      },
+  async getContract() {
+    const jsonFile = fs.readFileSync('src/web3/nex.json', 'utf8');
+    const jsonData = JSON.parse(jsonFile);
+    const contract = new ethers.Contract(
+      process.env.GASLESS_NFT_RECIPIENT_CONTRACT_ADDRESS,
+      jsonData.abi,
+      // this.provider,
+      this.wallet,
     );
 
-    // Update metadata
-    const lastTokenId = (await this.contract.getTotalSupply()) - 1;
-    await this.contract.setBaseURI(
-      lastTokenId,
-      'ipfs://QmXEyPiccKn1ifXyJQohnRgtanxpvBdwzPbE2na4LvwQx3',
-    );
-
-    // Transfer NFT
-    const signer = new ethers.Wallet(
-      process.env.RUNNER_PRIVATE_KEY,
-      this.provider,
-    );
-
-    await this.contract.safeTransferFrom(signer.address, address, lastTokenId);
+    return contract;
   }
 
-  async callEvent(address: string) {
-    this.contract.eventFrize(address);
+  async mintNFT(body): Promise<void> {
+    try {
+      let nonce = this.getRecentNonce();
+
+      const contract = await this.getContract();
+
+      const mint = await contract.mint();
+      console.log(mint);
+      nonce = mint.nonce + 1;
+
+      const lastTokenId = Number(await contract.getTotalSupply()) - 1;
+      console.log('lastTokenId: ', lastTokenId);
+
+      await this.prisma.user.update({
+        where: {
+          address: body.address,
+        },
+        data: {
+          tokenId: lastTokenId,
+        },
+      });
+
+      await this.setMetadata(body.address, lastTokenId);
+
+      const result = await contract.safeTransferFrom(
+        this.wallet.address,
+        body.address,
+        lastTokenId,
+      );
+      nonce = result.nonce + 1;
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  async callEvent(eventRequest) {
+    const contract = await this.getContract();
+    let nonce = await this.getRecentNonce();
+
+    const result = await contract.eventPrize(
+      eventRequest.address,
+      eventRequest.prize,
+    );
+    console.log(result);
+    nonce = result.nonce + 1;
+
+    const tokenId = (
+      await this.prisma.user.findUnique({
+        where: {
+          address: eventRequest.address,
+        },
+      })
+    ).tokenId;
+
+    this.setMetadata(eventRequest.address, tokenId);
+  }
+
+  async setMetadata(address, tokenId) {
+    const metadata = [
+      'QmdbTNfVtBWfzYq54znSQY8M4Lxs1erYzUkBXd2r2c1K8B',
+      'QmaLvwnbURubSXoLoNnjjD5kktuHU4FxUqUHhM13fpjsAs',
+      'QmSg8ZNZnbMUMx7yVqXkDuv7qJjTMHeCiKgQSSaLoRZaSx',
+    ];
+    const contract = await this.getContract();
+    let nonce = await this.getRecentNonce();
+
+    const grade = (
+      await this.prisma.user.findUnique({
+        where: {
+          address: address,
+        },
+      })
+    ).grade;
+
+    const setBaseUri = await contract.setBaseURI(
+      tokenId,
+      'ipfs://' + `${metadata[grade - 1]}`,
+      { nonce: nonce },
+    );
+    nonce = setBaseUri.nonce + 1;
+
+    console.log(setBaseUri);
+
+    const tokenURI = await contract.tokenURI(tokenId - 1);
+    console.log('tokenURI: ', tokenURI);
+
+    await this.prisma.user.update({
+      where: {
+        address: address,
+      },
+      data: {
+        grade: grade + 1,
+      },
+    });
+  }
+
+  async getRecentNonce() {
+    const nonce = await this.provider.getTransactionCount(
+      process.env.RUNNER_ADDRESS,
+      'latest',
+    );
+    return nonce + 1;
   }
 }
